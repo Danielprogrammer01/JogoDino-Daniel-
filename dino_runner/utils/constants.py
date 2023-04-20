6a43ea7477c7db1ef70ee0affbd656d03cb4027d
Danielprogrammer01 <130627373+Danielprogrammer01@users.noreply.github.com>
2023-04-20 10:30:28 -0300

    Add files via upload

---
+++ dino_runner/utils/constants.py
@@ -0,0 +1,91 @@
+import pygame
+import os
+
+# Global Constants
+TITLE = "Chrome Dino Runner"
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+FPS = 30
+IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
+X_POS_BG = 0
+Y_POS_BG = -90
+X_POS_MENU = 0
+Y_POS_MENU = 0
+GAME_SPEED = 20
+
+X_POS = 80
+Y_POS = 310
+
+Y_POS_DUCK = 340
+
+# Assets Constants
+ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png"))
+
+RUNNING = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
+]
+
+RUNNING_SHIELD = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1Shield.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
+]
+
+RUNNING_HAMMER = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Hammer.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
+]
+
+JUMPING = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJump.png"))
+JUMPING_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpShield.png"))
+
+# JUMPING_HAMMER = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpHammer.png"))
+
+DUCKING = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+]
+
+DUCKING_SHIELD = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Shield.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+]
+
+DUCKING_HAMMER = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Hammer.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+]
+
+
+SMALL_CACTUS = [
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus3.png")),
+]
+LARGE_CACTUS = [
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus3.png")),
+]
+
+BIRD = [
+    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird2.png")),
+]
+
+CLOUD = pygame.image.load(os.path.join(IMG_DIR, "Other/Cloud.png"))
+SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Other/shield.png"))
+HAMMER = pygame.image.load(os.path.join(IMG_DIR, "Other/hammer.png"))
+
+# Novo plano de fundo e menu
+
+BG = pygame.image.load(os.path.join(IMG_DIR, "Other/teladefundo.png"))
+MENU = pygame.image.load(os.path.join(IMG_DIR, "Other/menu.png"))
+
+
+HEART = pygame.image.load(os.path.join(IMG_DIR, "Other/SmallHeart.png"))
+
+JUMP_VEL = 8.5
+
+DEFAULT_TYPE = "default"
+SHIELD_TYPE = "shield"
